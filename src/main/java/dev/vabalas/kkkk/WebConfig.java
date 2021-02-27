bd304f550d084a6170ab09727b38d76a8bef7bad
GVabal <gvabal@gmail.com>
2021-02-27 14:53:32 +0200

    Fix WebConfig

--- src/main/java/dev/vabalas/kkkk/WebConfig.java
+++ src/main/java/dev/vabalas/kkkk/WebConfig.java
@@ -1,3 +1,9 @@
+package dev.vabalas.kkkk;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
   @Override
